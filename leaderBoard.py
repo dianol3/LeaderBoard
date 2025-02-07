f326c6337d290b91858f01a6e1df15bc12486885
dianol3 <135699030+dianol3@users.noreply.github.com>
2025-02-07 14:37:28 +0000

    Update leaderBoard.py

--- leaderBoard.py
+++ leaderBoard.py
@@ -21,7 +21,7 @@ def autoplay_audio(audio_url: str):
         st.markdown(md, unsafe_allow_html=True)
         
 # Caminho do áudio hospedado no GitHub
-audio_url = "https://raw.githubusercontent.com/dianol3/LeaderBoard/main/whistle.mp3.mp3"
+audio_url = https://github.com/dianol3/LeaderBoard/blob/master/whistle.mp3.mp3"
 
 
 
